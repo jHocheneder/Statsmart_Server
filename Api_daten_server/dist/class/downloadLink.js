fba84b6b08c58d58e26bdd05a57bcd6025f98f49
Jakob Hocheneder <jakob.hocheneder@gmail.com>
2020-12-14 20:52:02 +0100

    Cors

---
+++ Api_daten_server/dist/class/downloadLink.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DownloadLink = void 0;
+class DownloadLink {
+    constructor() { }
+}
+exports.DownloadLink = DownloadLink;
+//# sourceMappingURL=downloadLink.js.map
